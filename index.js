0145cf316db2d191437a784fbe6d92a21a51662e
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-11-11 14:49:26 +0530

    creating the express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Welcome to inventry app");
+});
+
+server.listen(3000, () => console.log("Server is listening on port 3000"));
