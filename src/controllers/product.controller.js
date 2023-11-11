b9afd3078c8d9b17037fd61c5193c4a3d7a394c9
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-11-11 22:50:32 +0530

    added validation to the form

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,10 +11,26 @@ export default class ProductController {
         // );
     }
     getAddForm(req, res) {
-        res.render("new-product");
+        res.render("new-product", { errorMessage: null });
     }
     addNewProduct(req, res) {
+        // Validate data
         const { name, desc, price, imageUrl } = req.body;
+        let errors = [];
+        if (!name || name.trim() == "") {
+            errors.push("Name is required");
+        }
+        if (!price || parseFloat(price) < 1) {
+            errors.push("Price must be a positive number");
+        }
+        try {
+            const validUrl = new URL(imageUrl);
+        } catch (err) {
+            errors.push("Url is invalid");
+        }
+        if (errors.length > 0) {
+            return res.render("new-product", { errorMessage: errors[0] });
+        }
         const product = ProductModel.add(name, desc, price, imageUrl);
         let products = ProductModel.get();
         res.render("products", { products: products });
