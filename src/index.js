6b7ddb4ff0742fd52e2597861d40c159697e46bd
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-11-11 15:36:35 +0530

    added product model and retrived data in the controllere

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
-// const express = require('express');
 
 const server = express();
 
 // create an instance of ProductController
 const productController = new ProductController();
+
+// Get method
 server.get("/", productController.getProducts);
+
+// Serving the static files
 server.use(express.static("src/views"));
-// return res.send('Welcome to Inventory App');
-server.listen(3400);
-console.log("Server is listening on pert 3400");
+
+// Listening the server
+server.listen(3400, () => console.log("Server is listening on pert 3400"));
